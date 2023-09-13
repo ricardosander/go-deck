0f5db86abbb8fdc86e9685aa99bb433872df77aa
Ricardo Lopes <ricardo.lopes@ifood.com.br>
2023-09-13 12:17:16 -0300

    Improve code based on new deck type

--- deck.go
+++ deck.go
@@ -4,6 +4,21 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	var cardSuits = []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	var cardsValues = []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardsValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
