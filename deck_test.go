a9db3393703a07f78b93ea6de26497d883a051d4
Ricardo Lopes <ricardo.lopes@ifood.com.br>
2023-09-30 21:48:19 -0300

    Add more tests

--- deck_test.go
+++ deck_test.go
@@ -10,4 +10,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected NewDeck lenth of 52, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first element to be Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected first element to be Ace of Spades, but got %v", d[len(d)-1])
+	}
 }
