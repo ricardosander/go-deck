fd6e9e9da0403de4e0645b2fa18347c3259f7795
Ricardo Lopes <ricardo.lopes@ifood.com.br>
2023-09-30 22:01:06 -0300

    Add more tests

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -18,4 +19,22 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Clubs" {
 		t.Errorf("Expected first element to be Ace of Spades, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	testFileName := "_decktesting"
+	_ = os.Remove(testFileName)
+
+	deck := newDeck()
+	err := deck.saveToFile(testFileName)
+	if err != nil {
+		t.Errorf("Error when saving to file: %v", err)
+	}
+
+	deck = newDeckFromFile(testFileName)
+	if len(deck) != 52 {
+		t.Errorf("Expected newDeckFromFile lenth of 52, but got %v", len(deck))
+	}
+
+	_ = os.Remove(testFileName)
 }
